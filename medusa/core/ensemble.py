a9746fb37c4ca4e44f45a1907e591aa029141cb5
gregmedlock <gmedlo@gmail.com>
2018-03-23 16:23:21 -0400

    refactoring features DF to handle multiple features for the same model identifier (e.g. multiple features for the same reaction ID)

--- medusa/core/ensemble.py
+++ medusa/core/ensemble.py
@@ -200,8 +200,8 @@ class Ensemble:
         model_states = self.states.loc[model_id]
         for reaction in model_states.index:
             if model_states.loc[reaction]:
-                rxn.lower_bound = self.features[reaction]['lb']
-                rxn.upper_bound = self.features[reaction]['ub']
+                rxn.lower_bound = self.features.loc[reaction,'lower_bound']
+                rxn.upper_bound = self.features.loc[reaction,'upper_bound']
             else:
                 rxn.lower_bound = 0
                 rxn.upper_bound = 0
@@ -222,49 +222,30 @@ class Ensemble:
 
         return base_model
 
-    # def _create_reaction_diffs(self,model_list):
-    #     '''
-    #
-    #
-    #     '''
-    #     # Get list of every reaction present in all models
-    #     reaction_set = set()
-    #     for model in model_list:
-    #         model_reactions = set([reaction.id for reaction in model.reactions])
-    #         if len(reaction_set) > 0:
-    #             reaction_set = reaction_set & model_reactions
-    #         else:
-    #             reaction_set = model_reactions
-    #     reaction_list = list(reaction_set)
-    #
-    #     # Get list of every reaction that isn't common to all models
-    #     variable_reactions = set()
-    #     for model in model_list:
-    #         reactions_different = [rxn.id for rxn in model.reactions if rxn.id not in reaction_list]
-    #         if len(variable_reactions) > 0:
-    #             variable_reactions = variable_reactions | set(reactions_different)
-    #         else:
-    #             variable_reactions = set(reactions_different)
-    #     variable_reaction_list = list(variable_reactions)
-    #
-    #
-    #     # For every reaction that isn't common to all models, find the
-    #     # upper and lower bound. Reactions that aren't present in a model
-    #     # have upper and lower bounds of 0 to prevent any flux.
-    #     reaction_diffs = {}
-    #     for model in model_list:
-    #         reaction_diffs[model.id] = {}
-    #         for reaction in variable_reaction_list:
-    #             if reaction in [rxn.id for rxn in model.reactions]:
-    #                 rxn_in_model = model.reactions.get_by_id(reaction)
-    #                 lb = rxn_in_model.lower_bound
-    #                 ub = rxn_in_model.upper_bound
-    #                 reaction_diffs[model.id][reaction] = {'lb':lb,'ub':ub}
-    #             else:
-    #                 # if the reaction wasn't in the model, it should be closed
-    #                 # in both directions for this member of the ensemble
-    #                 reaction_diffs[model.id][reaction] = {'lb':0.0,'ub':0.0}
-    #     return reaction_diffs
+    def _update_features(self,new_ensemble):
+
+        # get the features in the new ensemble base_model missing from the
+        # current base_model
+        old_base_rxns = [rxn.id for rxn in self.base_model.reactions]
+        new_base_rxns = [rxn.id for rxn in new_ensemble.base_model.reactions]
+        # find reactions in the new base that aren't in the old_base
+        new_base_notin_old_base = list(set(new_base_rxns) - set(old_base_rxns))
+
+        # find variable features in the new ensemble that weren't variable in
+        # the old ensemble
+        new_features = new_ensemble.features.index.tolist()
+        old_features = self.features.index.tolist()
+        new_features_notin_old_features = list(set(new_features) - set(old_features))
+
+        features_to_add = list(set(new_base_notin_old_base) & set(new_features_notin_old_features))
+
+
+
+    def _update_states(self,new_ensemble):
+        'placeholder'
+
+    def _update_base(self,new_ensemble):
+        'placeholder'
 
 
 def set_features_states(ensemble,model_list):
@@ -301,13 +282,22 @@ def set_features_states(ensemble,model_list):
             reactions_different = [rxn for rxn in model.reactions if rxn.id not in reaction_list]
             for reaction in reactions_different:
                 if not reaction.id in variable_reactions.keys():
-                    variable_reactions[reaction.id] = {'lb':model.reactions.get_by_id(reaction.id).lower_bound,\
-                                                        'ub':model.reactions.get_by_id(reaction.id).upper_bound}
+                    variable_reactions[reaction.id] = {'lower_bound':model.reactions.get_by_id(reaction.id).lower_bound,\
+                                                        'upper_bound':model.reactions.get_by_id(reaction.id).upper_bound,\
+                                                        'type':'reaction',\
+                                                        'model_identifier':reaction.id}
             model_states = pd.DataFrame({feature:True for feature in variable_reactions.keys()},index=[model.id])
             features = pd.DataFrame(variable_reactions).T
+            ### STILL NEED TO GET CORRECT FEATURE IDs BACK TO THE STATE DATAFRAME FOR REDUNDANT FEATURES
+
             if len(ensemble.features) > 0:
                 new_df = pd.concat([ensemble.features,features])
-                ensemble.features = new_df[~new_df.index.duplicated(keep='first')]
+                duplicate_features = new_df.duplicated()
+                new_df = new_df[~duplicated_features] # remove true duplicates
+                new_df.index = new_df.index + \
+                            new_df.groupby('model_identifier').cumcount().astype(str) # add integer to index based on the feature being assigned to the
+                            # same identifier within the model (e.g. same reaction id)
+                ensemble.features = new_df
                 new_df = pd.concat([ensemble.states,model_states])
                 ensemble.states = new_df.fillna(False)
             else:
@@ -329,9 +319,140 @@ def update_features_states(current_ensemble,model_list):
 
     # create a new ensemble from the input list
     new_ensemble = Ensemble(base_id='new_models',model_list=model_list)
+    current_ensemble._update_features(new_ensemble)
+    current_ensemble._update_states(new_ensemble)
+    current_ensemble._update_base(new_ensemble)
+
+    # tasks:
+    # 1. update base with new reactions
+    # 2. add reactions to features that are now newly variable and update model states for new feature
+    # 3.
+    # determine reactions that need to be added to the diff (present in new ensemble and not in base)
+
+
 
     # merge the old ensemble with the new ensemble
     all_features = pd.concat([current_ensemble.features,new_ensemble.features])
     current_ensemble.features = all_features[~all_features.index.duplicated(keep='first')]
     all_states = pd.concat([current_ensemble.states,new_ensemble.states])
     current_ensemble.states = all_states.fillna(False)
+
+def add_models(current_ensemble,model_list):
+    '''
+    Adds an additional model to an existing ensemble. Modifies base_model to
+    include any unique reactions and updates the reaction diff.
+
+    When creating an ensemble for more than hundreds of models, creating an
+    empty base model, then adding the models individually using add_models
+    can prevent the need to load all models simultaneously (e.g. you can
+    load and add one at a time, then clear the individual model from memory).
+    This methods conserves memory relative to Ensemble_model.init, but may take
+    slightly longer to run as the reaction diff is recalculated for each
+    addition and more calls to base_model.add_reactions() are made.
+
+    WARNING: currently requires starting from an ensemble object which
+    contains at least one base model. If an empty ensemble is used with
+    add_models(), the diff is considered to be every reaction. We should
+    refactor to enable starting with an empty model, since this makes
+    code a little cleaner for the user.
+    '''
+
+    if len(current_ensemble.states) < 1:
+        # If empty, just generate new base and diffs from input models
+        new_base_model = current_ensemble._create_base_model(model_list=model_list)
+        #new_reaction_diffs = self._create_reaction_diffs(model_list=model_list)
+        current_ensemble.base_model = new_base_model
+        set_features_states(current_ensemble,model_list=model_list)
+        #self.reaction_diffs = new_reaction_diffs
+    else:
+        new_ensemble = Ensemble(model_list=model_list)
+        #new_reaction_diffs = self._create_reaction_diffs(model_list=model_list)
+
+        old_base_rxns = [rxn.id for rxn in current_ensemble.base_model.reactions]
+        new_base_rxns = [rxn.id for rxn in new_ensemble.base_model.reactions]
+        # currently grabs the reaction list from the first model. Requires update
+        # if reaction_diff is refactored.
+        old_features = old_ensemble.features.index.tolist()
+        new_features = new_ensemble.features.index.tolist()
+
+        # find reactions in the new base that aren't in the old_base
+        new_base_notin_old_base = list(set(new_base_rxns) - set(old_base_rxns)) # these should be in new diff
+        # find reactions in the old base that aren't in the new base
+        old_base_notin_new_base = list(set(old_base_rxns) - set(new_base_rxns)) # these should be in new diff
+        # find reactions present in both new and old bases.
+        in_both_bases = list(set(old_base_rxns) & set(new_base_rxns)) # these are the new universal reactions (if they have the same bounds)
+
+        rxns_to_add_to_old_base = []
+        for reaction in new_base_notin_old_base:
+            rxns_to_add_to_old_base.append(new_ensemble.base_model.reactions.get_by_id(reaction))
+            for model in current_ensemble.states.index.tolist():
+                # close the reaction in the old reaction diffs
+                #self.reaction_diffs[model][reaction] = {'lb':0.0,'ub':0.0}
+                current_ensemble.states.loc[model,reaction] = False
+            if reaction not in new_features:
+                # if the reaction wasn't in the new features, we need to add it with
+                # the bounds from the new base since the reaction is absent from the old models
+                lb_in_new_base = new_ensemble.base_model.reactions.get_by_id(reaction).lower_bound
+                ub_in_new_base = new_ensemble.base_model.reactions.get_by_id(reaction).upper_bound
+                for model in new_ensemble.states.index.tolist():
+                    new_reaction_diffs[model][reaction] = \
+                        {'lb':lb_in_new_base,'ub':ub_in_new_base}
+
+        for reaction in old_base_notin_new_base:
+            if reaction not in old_diff_rxns:
+                # if the reaction isn't in the old diff but isn't in the new base,
+                # we need to add it to the diff with
+                # the bounds from old_reaction_diff since it isn't in the new base.
+                # (e.g. the reaction is now different in some models)
+                lb_in_old_base = self.base_model.reactions.get_by_id(reaction).lower_bound
+                ub_in_old_base = self.base_model.reactions.get_by_id(reaction).upper_bound
+                for model in self.reaction_diffs.keys():
+                    self.reaction_diffs[model][reaction] = \
+                        {'lb':lb_in_old_base,'ub':ub_in_old_base}
+                # add the reaction to the new diffs, with it closed since it
+                # wasn't in the new base.
+                for model in new_reaction_diffs.keys():
+                    new_reaction_diffs[model][reaction] = {'lb':0.0,'ub':0.0}
+            else:
+                #if the reaction was in the old_diff, we must add it to new_diff since it's
+                # variable in a set of models
+                if not reaction in new_diff_rxns: # this should always be true since these reactions aren't in the new base, but just in case.
+                    for model in new_reaction_diffs.keys():
+                        new_reaction_diffs[model][reaction] = {'lb':0.0,'ub':0.0}
+
+        for reaction in in_both_bases:
+            if reaction in old_diff_rxns:
+                if reaction not in new_diff_rxns:
+                    # add to new diff with bounds from new_base
+                    lb_in_new_base = new_base_model.reactions.get_by_id(reaction).lower_bound
+                    ub_in_new_base = new_base_model.reactions.get_by_id(reaction).upper_bound
+                    for model in new_reaction_diffs.keys():
+                        new_reaction_diffs[model][reaction] = \
+                            {'lb':lb_in_new_base,'ub':ub_in_new_base}
+            if reaction in new_diff_rxns:
+                if reaction not in old_diff_rxns:
+                    # add to old diff with bound from old_base
+                    lb_in_old_base = self.base_model.reactions.get_by_id(reaction).lower_bound
+                    ub_in_old_base = self.base_model.reactions.get_by_id(reaction).upper_bound
+                    for model in self.reaction_diffs.keys():
+                        self.reaction_diffs[model][reaction] = \
+                            {'lb':lb_in_old_base,'ub':ub_in_old_base}
+            else:
+                # if the reaction is in neither diff, check whether the bounds
+                # are the same in both old and new base, then update diffs
+                lb_in_new_base = new_base_model.reactions.get_by_id(reaction).lower_bound
+                ub_in_new_base = new_base_model.reactions.get_by_id(reaction).upper_bound
+                lb_in_old_base = self.base_model.reactions.get_by_id(reaction).lower_bound
+                ub_in_old_base = self.base_model.reactions.get_by_id(reaction).upper_bound
+                if lb_in_new_base != lb_in_old_base or ub_in_new_base != ub_in_old_base:
+                    for model in self.reaction_diffs.keys():
+                        self.reaction_diffs[model][reaction] = \
+                            {'lb':lb_in_old_base,'ub':ub_in_old_base}
+                    for model in new_reaction_diffs.keys():
+                        new_reaction_diffs[model][reaction] = \
+                            {'lb':lb_in_new_base,'ub':ub_in_new_base}
+        # update the base_model and reaction_diff
+        self.base_model.add_reactions(rxns_to_add_to_old_base)
+        self.base_model.repair()
+        for model in new_reaction_diffs.keys():
+            self.reaction_diffs[model] = new_reaction_diffs[model]
