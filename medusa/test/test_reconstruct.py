eae72d4300db6a0897817faf3102f6a093f4a68d
gregmedlock <gmedlo@gmail.com>
2018-08-27 13:03:59 -0400

    initial commit of continuous gapfilling, removed deprecated test models from old base/diff style

---
+++ medusa/test/test_reconstruct.py
@@ -0,0 +1,5 @@
+from cobra.test import create_test_model
+from medusa.core.ensemble import Ensemble
+
+REACTION_ATTRIBUTES = ['lower_bound', 'upper_bound']
+MISSING_ATTRIBUTE_DEFAULT = {'lower_bound':0,'upper_bound':0}
