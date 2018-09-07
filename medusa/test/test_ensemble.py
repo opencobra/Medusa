92cc53f0e7c647cbb613859db3ad44691f6b196a
gregmedlock <gmedlo@gmail.com>
2018-09-07 15:37:24 -0400

    added missing pickle load import in tests

--- medusa/test/test_ensemble.py
+++ medusa/test/test_ensemble.py
@@ -1,6 +1,8 @@
 from cobra.test import create_test_model
 from medusa.core.ensemble import Ensemble
 
+from pickle import load
+
 REACTION_ATTRIBUTES = ['lower_bound', 'upper_bound']
 MISSING_ATTRIBUTE_DEFAULT = {'lower_bound':0,'upper_bound':0}
 
